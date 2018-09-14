93b9a9328ba0ccdfd6e2d2d1062e7f1086cc0d7b
cherryxiu <1150445581@qq.com>
2018-09-14 18:59:02 +0800

    commit

--- src/components/tab/tab.vue
+++ src/components/tab/tab.vue
@@ -4,7 +4,7 @@
       <span class="tab-link">推荐</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/singer">
-      <span class="tab-link">歌手</span>
+      <span class="tab-link">歌单</span>
     </router-link>
     <router-link tag="div" class="tab-item" to="/rank">
       <span class="tab-link">排行
