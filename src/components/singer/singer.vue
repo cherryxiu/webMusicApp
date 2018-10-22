65f235ae336c9a2788e8142e1e3f30eda08b6e9f
cherryxiu <1150445581@qq.com>
2018-10-22 16:37:05 +0800

    获取歌手列表

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="singer" ref="singer">
-    <list-view @select="selectSinger" :data="singers" ref="list"></list-view>
+   <!-- <list-view @select="selectSinger" :data="singers" ref="list"></list-view>-->
     <router-view></router-view>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
 export default {
+  data () {
+    return {
+      singers: []
+    }
+  },
+  created () {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList () {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+        }
+      })
+    }
+  }
 }
 
 </script>
