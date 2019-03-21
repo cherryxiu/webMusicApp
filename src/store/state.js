9ecc029413cb75a2a6844f758e5b75ef1cf0e253
cherryxiu <1150445581@qq.com>
2019-03-21 15:31:43 +0800

    delete unused file

--- src/store/state.js
+++ src/store/state.js
@@ -1,5 +1,4 @@
 import {playMode} from 'common/js/config'
-import {loadSearch, loadPlay, loadFavorite} from 'common/js/cache'
 
 const state = {
   singer: {},
@@ -8,12 +7,7 @@ const state = {
   playlist: [], // 播放list  需要上一首,下一首跳转
   sequenceList: [], // 顺序列表(支持多种播放模式)
   mode: playMode.sequence,
-  currentIndex: -1, // 当前播放歌曲索引
-  disc: {},
-  topList: {},
-  searchHistory: loadSearch(),
-  playHistory: loadPlay(),
-  favoriteList: loadFavorite()
+  currentIndex: -1 // 当前播放歌曲索引
 }
 
 export default state
